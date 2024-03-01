793ebf9f63b13b59e0e9cb3fd178e2b1e3779195
Richard Rodriguez <rmjuarez12@gmail.com>
2024-02-29 22:36:50 -0500

    Adjusted main layout of website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>page</h1>;
+  return (
+    <>
+      <h1>PAge Title</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </>
+  );
 }
