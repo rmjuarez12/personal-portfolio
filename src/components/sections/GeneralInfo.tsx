09c70192de9170594ca0d4d2b93e370cfa422f6a
Richard Rodriguez <rmjuarez12@gmail.com>
2024-03-02 18:27:46 -0500

    Added social media buttons

--- src/components/sections/GeneralInfo.tsx
+++ src/components/sections/GeneralInfo.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 import Button from "../ui/Button";
 
 import { FaDownload } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { MdEmail } from "react-icons/md";
+import { IoLocationSharp } from "react-icons/io5";
 import InfoCard from "../ui/InfoCard";
+import IconBox from "../ui/IconBox";
 
 export default function GeneralInfo() {
   return (
@@ -10,37 +15,48 @@ export default function GeneralInfo() {
       <Image
         className='rounded-xl shadow-slate-100 shadow-sm -mt-32 mx-auto mb-6'
         src='/static/images/profile-pic.jpg'
-        alt='Richard Rodriguez'
+        alt='Richard Rodriguez Photo'
         width={200}
         height={200}
       />
 
       <h2 className='text-2xl mb-4 text-center'>Richard Rodriguez</h2>
 
-      <div className='mb-5 bg-slate-800 rounded'>
-        {/* <InfoCard
-          icon={<FaDownload />}
-          label={"Phone"}
-          description={"786.380.0965"}
-          isLast={false}
-        /> */}
+      <div className='flex flex-row justify-center mb-6'>
+        <IconBox
+          icon={<FaLinkedinIn />}
+          title='Linkedin'
+          url='https://www.linkedin.com/in/rmjuarez12/'
+        />
+        <IconBox
+          icon={<FiGithub />}
+          title='Github'
+          url='https://github.com/rmjuarez12'
+        />
+      </div>
 
+      <div className='mb-5 bg-slate-800 rounded'>
         <InfoCard
-          icon={<FaDownload />}
+          icon={<MdEmail />}
           label={"Email"}
           description={"rmjuarez12@gmail.com"}
           isLast={false}
         />
 
         <InfoCard
-          icon={<FaDownload />}
+          icon={<IoLocationSharp />}
           label={"Location"}
           description={"USA"}
           isLast={true}
         />
       </div>
 
-      <Button btnTxt='Download CV' newTab={true} icon={<FaDownload />} />
+      <Button
+        btnTxt='Download CV'
+        newTab={true}
+        icon={<FaDownload />}
+        url='https://docs.google.com/document/d/1xidQwUwknISKJSz7VXxoOEwOoxuBUdzvqgez8rAGSGU/edit?usp=sharing'
+      />
     </>
   );
 }
