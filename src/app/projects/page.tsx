d8b8abed058f612f2bf1dcdf44d82120d595cf2c
Richard Rodriguez <rmjuarez12@gmail.com>
2024-03-07 00:15:58 -0500

    Added More Projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,12 +10,41 @@ export default function Projects() {
 
       <div>
         <ProjectBox
+          className='mb-6'
           imageUrl='/static/images/vg-deals-preview.jpg'
           title='Steam Video Game Deals'
           description='In this webapp, you can search for games that are on sale, based on the price range of your choosing.'
-          technologies={["ReactJS", "React Redux"]}
+          technologies={["ReactJS", "React Redux", "JavaScript", "JSON"]}
           codeURL='https://github.com/rmjuarez12/Steam-Videogame-Deals'
           projectUrl='https://vg-deals.netlify.app/'
+        />
+
+        <ProjectBox
+          imageUrl='/static/images/react-photo-nasa.jpg'
+          title='NASA Photo of the day'
+          description="This is a webapp that displays a photo from NASA's API. You can cycle through the days and get the photo of that day!"
+          technologies={["ReactJS", "JSON", "JavaScript"]}
+          codeURL='https://github.com/rmjuarez12/nasa-photo-of-the-day/tree/Richard-Rodriguez'
+          projectUrl='https://react-photo-nasa-richard.netlify.app/'
+        />
+
+        <ProjectBox
+          className='mb-6'
+          imageUrl='/static/images/pokedex-og-150-app.jpg'
+          title='Pokedex'
+          description='This is a webapp that showcases the original 150 pokemon, with a Pokedex when you check their info. Project was done as learning experience on React.'
+          technologies={["ReactJS", "JSON", "JavaScript"]}
+          codeURL='https://github.com/rmjuarez12/web-sprint-challenge-intro-to-react/tree/Richard-Rodriguez'
+          projectUrl='https://pokedex-og-150.netlify.app/'
+        />
+
+        <ProjectBox
+          className='mb-6'
+          imageUrl='/static/images/wavykickzco.jpg'
+          title='Wavy Kickz Website'
+          description='This is a Shopify website, with a fully custom-made theme. The theme was built using Shopify Liquid language, based off of a PSD mockup.'
+          technologies={["Shopify", "Liquid", "JavaScript", "jQuery"]}
+          projectUrl='https://wavykickzco.com/'
         />
       </div>
     </>
