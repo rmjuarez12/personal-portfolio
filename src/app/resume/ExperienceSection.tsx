4a1d2bfaa28a891462ef682e1697a0a821480b44
Richard Rodriguez <rmjuarez12@gmail.com>
2025-11-07 16:34:42 -0500

    Fixed an issue with an import

--- src/app/resume/ExperienceSection.tsx
+++ src/app/resume/ExperienceSection.tsx
@@ -13,24 +13,15 @@ export default function ExperienceSection() {
       description: (
         <>
           <ul>
-            <li>
-              Worked closely on converting Figma design into a functional
-              ReactJS app.
-            </li>
-            <li>
-              Worked in a team in creating a mobile app based on Flutter
-              software development kit.
-            </li>
-            <li>
-              I contributed to the backend development team of an ExpressJS
-              application.
-            </li>
+            <li>Worked closely on converting Figma design into a functional ReactJS app.</li>
+            <li>Worked in a team in creating a mobile app based on Flutter software development kit.</li>
+            <li>I contributed to the backend development team of an ExpressJS application.</li>
           </ul>
         </>
       ),
     },
     {
-      date: "Jan 2020 - Present",
+      date: "Jan 2020 - Sept 2024",
       title: "Viola Media | Deer Park, NY",
       headlingURL: "https://viola.media/",
       subtitle: "Web Developer - Freelancer",
@@ -38,14 +29,8 @@ export default function ExperienceSection() {
         <>
           <ul>
             <li>Developed, coded and updated Wordpress and HTML sites.</li>
-            <li>
-              Perform maintenance on Wordpress websites, and ensure there are no
-              compatibility errors.
-            </li>
-            <li>
-              Used programming capabilities in PHP, SQL and JavaScript and other
-              libraries as needed.
-            </li>
+            <li>Perform maintenance on Wordpress websites, and ensure there are no compatibility errors.</li>
+            <li>Used programming capabilities in PHP, SQL and JavaScript and other libraries as needed.</li>
           </ul>
         </>
       ),
@@ -58,25 +43,12 @@ export default function ExperienceSection() {
       description: (
         <>
           <ul>
-            <li>
-              Tested websites and performed troubleshooting prior to deployment.
-            </li>
-            <li>
-              Optimized Images and Videos web assets for speed and performance.
-            </li>
+            <li>Tested websites and performed troubleshooting prior to deployment.</li>
+            <li>Optimized Images and Videos web assets for speed and performance.</li>
             <li>Developed, coded and updated Wordpress and HTML sites.</li>
-            <li>
-              Created site layout and user interface using HTML and CSS
-              practices.
-            </li>
-            <li>
-              Used programming capabilities in PHP, SQL and JavaScript and other
-              libraries as needed.
-            </li>
-            <li>
-              Created Wordpress custom themes from supplied layered files(PSD,
-              PDF, or AI).
-            </li>
+            <li>Created site layout and user interface using HTML and CSS practices.</li>
+            <li>Used programming capabilities in PHP, SQL and JavaScript and other libraries as needed.</li>
+            <li>Created Wordpress custom themes from supplied layered files(PSD, PDF, or AI).</li>
           </ul>
         </>
       ),
@@ -89,22 +61,11 @@ export default function ExperienceSection() {
       description: (
         <>
           <ul>
-            <li>
-              Created site layout and user interface using HTML and CSS
-              practices.
-            </li>
-            <li>
-              Created fully functional Wordpress themes based off of PSD files.
-            </li>
+            <li>Created site layout and user interface using HTML and CSS practices.</li>
+            <li>Created fully functional Wordpress themes based off of PSD files.</li>
             <li>Converted mockups into HTML, JavaScript, AJAX and JSON.</li>
-            <li>
-              Developed wireframes, site maps and content inventories for new
-              website designs.
-            </li>
-            <li>
-              Ensure all projects were fully functional prior to launching and
-              setting the site live.
-            </li>
+            <li>Developed wireframes, site maps and content inventories for new website designs.</li>
+            <li>Ensure all projects were fully functional prior to launching and setting the site live.</li>
           </ul>
         </>
       ),
@@ -117,14 +78,8 @@ export default function ExperienceSection() {
       description: (
         <>
           <ul>
-            <li>
-              Assisted with developing Websites in PHP/MySQL in order to work
-              with a custom CMS.
-            </li>
-            <li>
-              Assist with updating content, fixing any issues, and any
-              maintenance a website requires.
-            </li>
+            <li>Assisted with developing Websites in PHP/MySQL in order to work with a custom CMS.</li>
+            <li>Assist with updating content, fixing any issues, and any maintenance a website requires.</li>
             <li>Design/modify graphics when needed.</li>
           </ul>
         </>
@@ -134,13 +89,7 @@ export default function ExperienceSection() {
 
   return (
     <>
-      <Timeline
-        title='Experience'
-        timelineData={jobHistory}
-        icon={<MdWork />}
-        iconColor='text-lime-500'
-        className='mb-12'
-      />
+      <Timeline title="Experience" timelineData={jobHistory} icon={<MdWork />} iconColor="text-lime-500" className="mb-12" />
     </>
   );
 }
--- src/app/resume/SkillsSection.tsx
+++ src/app/resume/SkillsSection.tsx
@@ -2,18 +2,11 @@ import Heading from "@/components/ui/Heading";
 import IconBox from "@/components/ui/IconBox";
 import { SkillObj } from "@/utility/objTypes";
 
-import {
-  FaReact,
-  FaPhp,
-  FaNodeJs,
-  FaWordpressSimple,
-  FaShopify,
-  FaElementor,
-} from "react-icons/fa";
+import { FaReact, FaPhp, FaNodeJs, FaWordpressSimple, FaShopify, FaElementor } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { TiHtml5 } from "react-icons/ti";
 import { DiCss3 } from "react-icons/di";
-import { SiMysql, SiCsharp, SiExpress, SiTailwindcss } from "react-icons/si";
+import { SiMysql, SiSharp, SiExpress, SiTailwindcss } from "react-icons/si";
 import { TbBrandTypescript, TbBrandNextjs } from "react-icons/tb";
 import { BsStripe } from "react-icons/bs";
 import { VscGithub } from "react-icons/vsc";
@@ -42,7 +35,7 @@ export default function SkillsSection() {
     },
     {
       title: "C#",
-      icon: <SiCsharp />,
+      icon: <SiSharp />,
     },
   ];
 
@@ -110,53 +103,34 @@ export default function SkillsSection() {
   ];
 
   return (
-    <div className='mb-12'>
-      <Heading title='My Skills' subtitle='Tech Stack' showBorder={true} />
+    <div className="mb-12">
+      <Heading title="My Skills" subtitle="Tech Stack" showBorder={true} />
 
-      <h3 className='mb-5 text-3xl font-semibold'>Technical Languages</h3>
+      <h3 className="mb-5 text-3xl font-semibold">Technical Languages</h3>
 
-      <div className='flex flex-row mb-6 flex-wrap'>
+      <div className="flex flex-row mb-6 flex-wrap">
         {languages.map((skill: SkillObj, index: number) => {
           return (
-            <IconBox
-              key={index}
-              icon={skill.icon}
-              label={skill.title}
-              title={skill.title}
-              className='size-24 m-3'
-              iconClassName='text-4xl mb-2'
-            />
+            <IconBox key={index} icon={skill.icon} label={skill.title} title={skill.title} className="size-24 m-3" iconClassName="text-4xl mb-2" />
           );
         })}
       </div>
 
-      <h3 className='mb-5 text-3xl font-semibold'>
-        Technologies and Frameworks
-      </h3>
+      <h3 className="mb-5 text-3xl font-semibold">Technologies and Frameworks</h3>
 
-      <div className='flex flex-row flex-wrap mb-6'>
+      <div className="flex flex-row flex-wrap mb-6">
         {frameworksSet.map((skill: SkillObj, index: number) => {
           return (
-            <IconBox
-              key={index}
-              icon={skill.icon}
-              label={skill.title}
-              title={skill.title}
-              className='size-24 m-3'
-              iconClassName='text-4xl mb-2'
-            />
+            <IconBox key={index} icon={skill.icon} label={skill.title} title={skill.title} className="size-24 m-3" iconClassName="text-4xl mb-2" />
           );
         })}
       </div>
 
-      <h3 className='mb-5 text-3xl font-semibold'>General</h3>
+      <h3 className="mb-5 text-3xl font-semibold">General</h3>
 
       {generalSkills.map((skill: string, index: number) => {
         return (
-          <div
-            key={index}
-            className='text-lime-500 border border-lime-500 m-1 py-1 px-2 inline-block font-semibold bg-lime-950'
-          >
+          <div key={index} className="text-lime-500 border border-lime-500 m-1 py-1 px-2 inline-block font-semibold bg-lime-950">
             {skill}
           </div>
         );
