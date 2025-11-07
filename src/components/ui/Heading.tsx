1ca7545cfa0286fe3ae693bef8e796d427704c83
Richard Rodriguez <rmjuarez12@gmail.com>
2025-11-07 17:08:46 -0500

    Fixed issue with heading subtitle

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,18 +1,14 @@
 export default function Heading(props: HeadingProps) {
   return (
     <div className={`${props.className} mb-5`}>
-      <h3 className='md:text-5xl text-3xl mb-5 font-bold relative'>
-        {props.subtitle != null && (
-          <span className='select-none absolute -top-3/4 -left-5 md:text-7xl text-5xl z-0 opacity-5'>
-            {props.subtitle}
-          </span>
-        )}
-        <span className='z-20'>{props.title}</span>
+      <h3 className="md:text-5xl text-3xl mb-5 font-bold relative">
+        {props.subtitle != null && <span className="select-none absolute -top-7 -left-5 md:text-7xl text-5xl z-0 opacity-5">{props.subtitle}</span>}
+        <span className="z-20">{props.title}</span>
       </h3>
 
       {props.showBorder && (
-        <div className='h-1.5 w-40 bg-red-300 rounded-2xl relative'>
-          <div className='bg-red-600 absolute left-0 top-0 w-1/5 h-full rounded-2xl'></div>
+        <div className="h-1.5 w-40 bg-red-300 rounded-2xl relative">
+          <div className="bg-red-600 absolute left-0 top-0 w-1/5 h-full rounded-2xl"></div>
         </div>
       )}
     </div>
