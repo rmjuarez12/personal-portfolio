c5ad454475de16d88b368dcd34cbc231eaac69b5
Richard Rodriguez <rmjuarez12@gmail.com>
2025-11-07 16:43:03 -0500

    temporarily disabled CV button

--- src/components/sections/GeneralInfo.tsx
+++ src/components/sections/GeneralInfo.tsx
@@ -14,58 +14,35 @@ export default function GeneralInfo() {
   return (
     <>
       <Image
-        className='rounded-xl shadow-slate-100 shadow-sm -mt-32 mx-auto mb-6 w-auto h-auto'
-        src='/static/images/profile-pic.jpg'
-        alt='Richard Rodriguez Photo'
+        className="rounded-xl shadow-slate-100 shadow-sm -mt-32 mx-auto mb-6 w-auto h-auto"
+        src="/static/images/profile-pic.jpg"
+        alt="Richard Rodriguez Photo"
         width={200}
         height={200}
         priority={true}
       />
 
-      <h2 className='text-2xl mb-4 text-center'>Richard Rodriguez</h2>
+      <h2 className="text-2xl mb-4 text-center">Richard Rodriguez</h2>
 
-      <div className='flex flex-row justify-center mb-6'>
-        <IconBox
-          icon={<FaLinkedinIn />}
-          title='Linkedin'
-          url='https://www.linkedin.com/in/rmjuarez12/'
-        />
-        <IconBox
-          icon={<FiGithub />}
-          title='Github'
-          url='https://github.com/rmjuarez12'
-        />
+      <div className="flex flex-row justify-center mb-6">
+        <IconBox icon={<FaLinkedinIn />} title="Linkedin" url="https://www.linkedin.com/in/rmjuarez12/" />
+        <IconBox icon={<FiGithub />} title="Github" url="https://github.com/rmjuarez12" />
       </div>
 
-      <div className='mb-5 bg-slate-800 rounded'>
-        <InfoCard
-          icon={<MdEmail />}
-          label={"Email"}
-          description={"rmjuarez12@gmail.com"}
-          isLast={false}
-        />
+      <div className="mb-5 bg-slate-800 rounded">
+        <InfoCard icon={<MdEmail />} label={"Email"} description={"rmjuarez12@gmail.com"} isLast={false} />
 
-        <InfoCard
-          icon={<IoLocationSharp />}
-          label={"Location"}
-          description={"USA"}
-          isLast={false}
-        />
+        <InfoCard icon={<IoLocationSharp />} label={"Location"} description={"USA"} isLast={false} />
 
-        <InfoCard
-          icon={<RiSpeakFill />}
-          label={"I Fluently Speak"}
-          description={"English, Spanish"}
-          isLast={true}
-        />
+        <InfoCard icon={<RiSpeakFill />} label={"I Fluently Speak"} description={"English, Spanish"} isLast={true} />
       </div>
 
-      <Button
+      {/* <Button
         btnTxt='Download CV'
         newTab={true}
         icon={<FaDownload />}
         url='https://docs.google.com/document/d/1xidQwUwknISKJSz7VXxoOEwOoxuBUdzvqgez8rAGSGU/edit?usp=sharing'
-      />
+      /> */}
     </>
   );
 }
